d7be3625f6796eeb873343399e9827a99b369f48
Marlon Rosa <marlon.assinaturas@gmail.com>
2021-11-06 00:32:44 -0300

    eslint de test

--- ulhatravel.js
+++ ulhatravel.js
@@ -1,38 +1,40 @@
-const express = require('express')
+const express = require("express");
 
-const expressHandlebars= require('express-handlebars')
-const app = express()
-const handlers = require('./lib/handlers')
+const expressHandlebars= require("express-handlebars");
+const app = express();
+const handlers = require("./lib/handlers");
 
 //configura o view engine handlebars
-app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main',
-}))
+app.engine("handlebars", expressHandlebars({
+	defaultLayout: "main",
+}));
 
-app.set('view engine', 'handlebars')
+app.set("view engine", "handlebars");
 
 //const fortune = require('./lib/fortunes.js')
 
-app.get('/', handlers.home)
+app.get("/", handlers.home);
 
 
-app.get('/about', handlers.about)
+app.get("/about", handlers.about);
 
 //pagina 404 personalizada 
-app.use(handlers.notFound)
+app.use(handlers.notFound);
 
 // pagina 500 personalizada
-app.use(handlers.serverError)
+app.use(handlers.serverError);
 
 
-app.use(express.static(__dirname + '/public'))  
+// eslint-disable-next-line no-undef
+app.use(express.static(__dirname + "/public"));  
 
-const port = process.env.PORT || 3000
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000;
 if(require.main === module){
-    app.listen(port, ()=>{console.log(
-        `Express started on http://localhost:${port}; `
-        + `press Ctrl-C to terminate. `)})
-}else{ module.exports = app}
+	app.listen(port, ()=>{console.log(
+		`Express started on http://localhost:${port}; `
+        + "press Ctrl-C to terminate. ");});
+}else{ module.exports = app;}
 
     
 
